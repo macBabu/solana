a5dab9924a4ca629e7b9ae272180761ec6a27ffd
macBabu <156320901+macBabu@users.noreply.github.com>
2024-07-30 18:26:32 +0530

    solana app added

---
+++ solana-dapp/web/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default async function Index() {
+  return (
+    <div style={{height:'100%',width:'100%'}} >
+      <div style={{alignItems:'center',justifyContent:'center'}}>
+        <button style={{paddingBlock:6,paddingInline:10}} >
+          DO PING
+        </button>
+        </div>
+    </div>
+  );
+}
